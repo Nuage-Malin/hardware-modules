f8e9dc862b135deb04e2f751cd565dcd28016435
mathis darde <mathis.darde@epitech.eu>
2023-01-07 20:27:19 +0100

    on off dd for demo

--- controlHDD/controlHDD.go
+++ controlHDD/controlHDD.go
@@ -1,53 +1,51 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/warthog618/gpiod"
 )
 
-func getHardDiskRelay(hdd string, relay []int) *gpiod.Lines {
-	firstHDD, _ := gpiod.RequestLines(hdd, relay, gpiod.AsOutput(0, 1))
-
-	return firstHDD
-}
-
-func getInfoFromRelay(hdd *gpiod.Lines) bool {
-	err, _ := hdd.Info()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return err[0].Used
-}
-
-func hardDiskStatusManager(hdd string, relay []int) bool {
-	return getInfoFromRelay(getHardDiskRelay(hdd, relay))
-}
-
-func hardDiskShutDown(hdd *gpiod.Lines) {
-	err := hdd.Reconfigure(gpiod.WithLines([]int{5, 6}, gpiod.AsActiveLow))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func hardDiskStartUp(hdd *gpiod.Lines) {
-	err := hdd.Reconfigure(gpiod.WithLines([]int{5, 6}, gpiod.AsActiveHigh))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	hdd := "HDD1"
-	relay := []int{5, 6}
+	firstRelay, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	secondRelay, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	v := 0
+	ll, err := gpiod.RequestLines("gpiochip0", []int{firstRelay, secondRelay}, gpiod.AsOutput(v, v))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		ll.Reconfigure(gpiod.AsInput)
+		ll.Close()
+	}()
+	values := map[int]string{0: "inactive", 1: "active"}
+	fmt.Printf("Set pin %d %s\n", firstRelay, values[v])
+	fmt.Printf("Set pin %d %s\n", secondRelay, values[v])
 
-	if hardDiskStatusManager(hdd, relay) {
-		hardDiskShutDown(getHardDiskRelay(hdd, relay))
-	} else {
-		hardDiskStartUp(getHardDiskRelay(hdd, relay))
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(quit)
+
+	for {
+		select {
+		case <-time.After(2 * time.Second):
+			v ^= 1
+			ll.SetValues([]int{v, v})
+			fmt.Printf("Set pin %d %s\n", firstRelay, values[v])
+			fmt.Printf("Set pin %d %s\n", secondRelay, values[v])
+		case <-quit:
+			return
+		}
 	}
 }
