b3d10f107258c5aa70cc9d6f3ad0147db568743a
mathis darde <mathis.darde@epitech.eu>
2023-05-17 23:36:56 +0200

    get watts instead of kWh

--- getConso/getConso.go
+++ getConso/getConso.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math"
 	"strconv"
 	"strings"
 	"time"
@@ -14,7 +13,7 @@ import (
 )
 
 // var hddId = 0
-var DiskConsoList [1][4]float64 = [1][4]float64{} //voir pour moduler taille list
+var DiskConsoList [1][4]int = [1][4]int{} //voir pour moduler taille list
 
 // [0] : conso par seconde incr par seconde
 // [1] : nbr de seconde incr par seconde (if statement si 3600 sec)
@@ -42,7 +41,6 @@ func GetConso(hddId int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Print("connexion arduino\n")
 	scanner := bufio.NewScanner(s)
 	for scanner.Scan() {
 		if scanner.Err() != nil {
@@ -51,39 +49,30 @@ func GetConso(hddId int) {
 		if DiskConsoList[hddId][1] < 3600 {
 			str := scanner.Text()
 			split_str := strings.Split(str, " ")
-			// fmt.Print(split_str[0])
-			// fmt.Print("\n")
 			value, err := strconv.ParseFloat(split_str[0], 32)
 			if err != nil {
 				log.Fatal(err)
-			} else if DiskConsoList[hddId][3] == 0 {
-				time.Sleep(1000 * time.Millisecond) //à voir si nécessaire
-				DiskConsoList[hddId][0] += math.Round(value*100) / 100
-				DiskConsoList[hddId][1]++
-				DiskConsoList[hddId][2] += (DiskConsoList[hddId][0] / DiskConsoList[hddId][1]) // fonctionnel ?
-			} else {
-				time.Sleep(1000 * time.Millisecond) //à voir si nécessaire
-				DiskConsoList[hddId][0] += math.Round(value*100) / 100
-				DiskConsoList[hddId][1]++
 			}
+			time.Sleep(1000 * time.Millisecond) //à voir si nécessaire
+			DiskConsoList[hddId][0] = int(value)
+			DiskConsoList[hddId][1]++
+			// DiskConsoList[hddId][2] += (DiskConsoList[hddId][0] / DiskConsoList[hddId][1]) // fonctionnel ?
 		} else if DiskConsoList[hddId][1] >= 3600 { //trouver un moyen de quand meme envoyer des kW/h sans attendre directement 1 heure
-			DiskConsoList[hddId][2] += (DiskConsoList[hddId][0] / DiskConsoList[hddId][1])
+			//DiskConsoList[hddId][2] += (DiskConsoList[hddId][0] / DiskConsoList[hddId][1])
+			//DiskConsoList[hddId][1] = 0
 			DiskConsoList[hddId][1] = 0
-			DiskConsoList[hddId][0] = 0
 			DiskConsoList[hddId][3]++
 		}
-		// fmt.Print(DiskConsoList[hddId][0])
-		// fmt.Print("\n")
 	}
 }
 
-// arg int -> id hdd
-func SendConso(hddId int) float64 {
-	if DiskConsoList[hddId][3] < 2 {
-		return DiskConsoList[hddId][2]
-	} else {
-		return DiskConsoList[hddId][2] / DiskConsoList[hddId][3]
-	}
+func SendConso(hddId int) int {
+	// if DiskConsoList[hddId][3] < 2 {
+	// 	return DiskConsoList[hddId][2]
+	// } else {
+	// 	return DiskConsoList[hddId][2] / DiskConsoList[hddId][3]
+	// }
+	return DiskConsoList[hddId][0]
 }
 
 // func main() {
